18c62841ed51cf9244bbc480378aad208935c553
Carlos Legnaro de Almeida <carlos_legnaro@outlook.com>
2022-04-25 15:57:32 -0300

    inicio

---
+++ Model.Dao/ConexaoDB.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+
+namespace Model.Dao
+{
+    class ConexaoDB
+    {
+        private static ConexaoDB objConexaoDb = null;
+        private SqlConnection con;
+
+        private ConexaoDB()
+        {
+            con = new SqlConnection("Data Source=PC-CARLOS\\SQLSERVER2019;Initial Catalog=financeiro;Integrated Security=True");
+        }
+
+        public static ConexaoDB saberEstado()
+        {
+            if (objConexaoDb == null)
+            {
+                objConexaoDb = new ConexaoDB();
+            }
+
+            return objConexaoDb;
+        }
+
+        public SqlConnection getCon()
+        {
+            return con;
+        }
+
+        public void CloseDB()
+        {
+            objConexaoDb = null;
+        }
+    }
+}
---
+++ Model.Dao/Obrigatorio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Model.Dao
+{
+    public interface Obrigatorio<qualquerClasse>
+    {
+        void create(qualquerClasse obj);
+        void delete(qualquerClasse obj);
+        void update(qualquerClasse obj);
+        bool find(qualquerClasse obj);
+        List<qualquerClasse> findAll();
+    }
+}
